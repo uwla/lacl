0646a1c2e635ec3b8d534cc3e590d2c5fd132bfa
uwla <uwlalinux@gmail.com>
2023-02-13 09:16:29 -0300

    initial commit

---
+++ src/Foo.php
@@ -0,0 +1,15 @@
+<?php
+
+// just testing things...
+class Foo
+{
+    /**
+     * Create a new class instance
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        echo "constructed\n";
+    }
+}
