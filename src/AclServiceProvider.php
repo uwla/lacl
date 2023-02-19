3d5f2e92b6a875b154a6a58f90cf727420a2f929
uwla <uwlalinux@gmail.com>
2023-02-19 10:23:32 -0300

    release first alpha version
    After the tests passed, it is time to release the first alpha version.

--- src/AclServiceProvider.php
+++ src/AclServiceProvider.php
@@ -6,22 +6,8 @@ use Illuminate\Support\ServiceProvider;
 
 class AclServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        // if (! $this->app->runningInConsole())
-        //     return;
-
-        // publishes migration
-        $stub = '../database/migrations/create_acl_tables.php.stub';
-        $src  = __DIR__ . '/' . $stub;
-        $dest = database_path('migrations/' . date('Y_m_d_His', time()) . '_create_acl_tables.php');
-        $this->publishes([
-            $src => $dest
-        ], 'migrations');
-    }
-
     public function boot()
     {
-        //
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 }
