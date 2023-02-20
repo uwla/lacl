9e38aac229def5296dc90de32e6f1f1d497d4247
uwla <uwlalinux@gmail.com>
2023-02-20 15:48:15 -0300

    fix creating extra database folder when publishing migrations
    It  should  publish  migrations  to  database/migrations  but   it   was
    publishing to database/database/migrations. It should now be fixed.

--- src/AclServiceProvider.php
+++ src/AclServiceProvider.php
@@ -12,7 +12,7 @@ class AclServiceProvider extends ServiceProvider
         $src = __DIR__ . '/' . '../database/migrations/create_acl_tables.php';
         $now = date('Y_m_d_His', time());
         $dest = $this->app->databasePath(
-            'database/migrations/' . $now . '_create_acl_tables.php'
+            'migrations/' . $now . '_create_acl_tables.php'
         );
         $this->publishes([
             $src => $dest
