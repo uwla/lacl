a6c7cbfa4d3a43e1cffcfcf3878fe10b334c589d
uwla <uwlalinux@gmail.com>
2023-02-20 15:42:35 -0300

    publish migration files

--- src/AclServiceProvider.php
+++ src/AclServiceProvider.php
@@ -8,6 +8,14 @@ class AclServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        // publishes migrations
+        $src = __DIR__ . '/' . '../database/migrations/create_acl_tables.php';
+        $now = date('Y_m_d_His', time());
+        $dest = $this->app->databasePath(
+            'database/migrations/' . $now . '_create_acl_tables.php'
+        );
+        $this->publishes([
+            $src => $dest
+        ], 'migrations');
     }
 }
